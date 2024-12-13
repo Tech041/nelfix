8b455c50f000a129a5a4a16b1d5591585c970083
nelson <chinwubanelson@gmail.com>
2024-12-13 10:06:51 +0100

    modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavbarItem from "./NavbarItems";
-
+import { Suspense } from "react";
 
 const Navbar = () => {
   return (
     <div className="flex bg-purple-600 text-white p-4 lg:text-lg justify-center gap-6">
-      <NavbarItem title="Trending" param="fetchTrending" />
-      <NavbarItem title="Top Rated" param="fetchTopRated" />
+      <Suspense fallback={<div>Loading ....</div>}>
+        <NavbarItem title="Trending" param="fetchTrending" />
+        <NavbarItem title="Top Rated" param="fetchTopRated" />
+      </Suspense>
     </div>
   );
 };
