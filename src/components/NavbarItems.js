e2fafa526382f349d962965f32b7cb261dd385bb
nelson <chinwubanelson@gmail.com>
2024-12-13 11:30:28 +0100

    optimized

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -1,14 +1,26 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React from "react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const NavbarItem = ({ title, param }) => {
   const searchParams = useSearchParams();
-  const genre = searchParams.get('genre')
+  const genre = searchParams?.get("genre");
   return (
     <div>
-      <Link className={`${genre === param ? 'underline underline-offset-8 decoration-amber-500 rounded-lg':''}`} href={`/?genre=${param}`}>{title}</Link>
+      <Suspense>
+        <Link
+          className={`${
+            genre === param
+              ? "underline underline-offset-8 decoration-amber-500 rounded-lg"
+              : ""
+          }`}
+          href={`/?genre=${param}`}
+        >
+          {title}
+        </Link>
+      </Suspense>
     </div>
   );
 };
